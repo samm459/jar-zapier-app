3d0c02d186669fa7ae9089589ee5b27f3ab8c6c4
samm459 <sam@contentcucumber.com>
2020-04-14 11:22:55 -0400

    Add help text to authentication fields

--- authentication.js
+++ authentication.js
@@ -8,10 +8,12 @@ module.exports = {
   fields: [
     {
       key: 'api_key',
+      helpText: 'Found at the [API Details](https://app.jarhq.com/settings/api-details) page in your Jar account.',
       label: 'API Key', required: true, type: 'string'
     },
     {
       key: 'subdomain',
+      helpText: 'The [Jar App](https://app.jarhq.com) will automatically redirect you to your subdomain. For example, if you are redirected to mycompany.jarhq.com, your subdomain is mycompany.',
       label: 'API Key', required: true, type: 'string'
     }
   ],
