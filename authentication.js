3583e11c32bc38464aa08861934862be92de4025
samm459 <sam@contentcucumber.com>
2020-07-28 06:51:04 -0400

    Change modules to objects

--- authentication.js
+++ authentication.js
@@ -1,29 +1,25 @@
-class Authentication {
-  static test = {
-    url: "https://jarhq.com/api/customers/requests.json"
-  }
-
-  static connectionLabel(_, bundle) {
-    return bundle.inputData.subdomain;
-  }
-
-  static config = {
-    type: "custom",
-    fields: [
-      {
-        key: 'api_key',
-        helpText: 'Found at the [API Details](https://app.jarhq.com/settings/api-details) page in your Jar account.',
-        label: 'API Key', required: true, type: 'string'
-      },
-      {
-        key: 'subdomain',
-        helpText: 'The [Jar App](https://app.jarhq.com) will automatically redirect you to your subdomain. For example, if you are redirected to mycompany.jarhq.com, your subdomain is mycompany.',
-        label: 'API Key', required: true, type: 'string'
-      }
-    ],
-    test: Authentication.test,
-    connectionLabel: Authentication.connectionLabel
-  };
+const test = {
+  url: "https://jarhq.com/api/customers/requests.json"
 }
 
-module.exports = Authentication;
+function connectionLabel(_, bundle) {
+  return bundle.inputData.subdomain;
+}
+
+module.exports = {
+  type: "custom",
+  fields: [
+    {
+      key: 'api_key',
+      helpText: 'Found at the [API Details](https://app.jarhq.com/settings/api-details) page in your Jar account.',
+      label: 'API Key', required: true, type: 'string'
+    },
+    {
+      key: 'subdomain',
+      helpText: 'The [Jar App](https://app.jarhq.com) will automatically redirect you to your subdomain. For example, if you are redirected to mycompany.jarhq.com, your subdomain is mycompany.',
+      label: 'API Key', required: true, type: 'string'
+    }
+  ],
+  test: test,
+  connectionLabel: connectionLabel
+};
--- index.js
+++ index.js
@@ -1,35 +1,31 @@
-const Authentication = require('./Authentication');
-const RequestTrigger = require('./triggers/RequestTrigger');
-const InviteCreator = require('./creates/InviteCreator');
-const ClientResource = require("./resources/ClientResource");
+const authentication = require('./authentication');
+const request = require('./triggers/request');
+const invite = require('./creates/invite');
+const client = require("./resources/client");
 
-class App {
-  static resources = { [ClientResource.config.key]: ClientResource.config }
-  static triggers = { [RequestTrigger.config.key]: RequestTrigger.config }
-  static creates = { [InviteCreator.config.key]: InviteCreator.config }
+const resources = { [client.key]: client }
+const triggers = { [request.key]: request }
+const creates = { [invite.key]: invite }
 
-  static configureAuth(request, _, bundle) {
-    request.headers['x-auth-token'] = bundle.authData.api_key
-    request.url = request.url.replace(
-      /^https:\/\//,
-      'https://'
-      + bundle.authData.subdomain
-      + '.'
-    )
-    return request;
-  }
-
-  static config = {
-    version: require('./package.json').version,
-    platformVersion: require('zapier-platform-core').version,
-    authentication: Authentication.config,
-    beforeRequest: [App.configureAuth],
-    afterResponse: [],
-    resources: App.resources,
-    triggers: App.triggers,
-    creates: App.creates,
-    searches: {}
-  }
+function configureAuth(request, _, bundle) {
+  request.headers['x-auth-token'] = bundle.authData.api_key
+  request.url = request.url.replace(
+    /^https:\/\//,
+    'https://'
+    + bundle.authData.subdomain
+    + '.'
+  )
+  return request;
 }
 
-module.exports = App.config;
+module.exports = {
+  version: require('./package.json').version,
+  platformVersion: require('zapier-platform-core').version,
+  authentication: authentication,
+  beforeRequest: [configureAuth],
+  afterResponse: [],
+  resources: resources,
+  triggers: triggers,
+  creates: creates,
+  searches: {}
+}
